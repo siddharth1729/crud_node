2ba236508338d1373f2a391eff6e2aca8a500100
siddharth <siddharth.sabron@gmail.com>
2022-06-27 03:18:42 +0530

    Changes

--- db.js
+++
@@ -1,8 +0,0 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://siddharth1729:@Pandey1729@cluster0.nik2p.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
